49896c7455705d49122ee148603fb1eec8ac6b59
Tegon Fluxo <fluxo.tegon@gmail.com>
2025-09-10 19:39:13 +0000

    na pagina inicial onde tem escrito fit by pam vamos mudar para Pam360 es

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,10 +5,10 @@ export function Logo({ className }: { className?: string }) {
     <div className={cn("inline-block", className)}>
       <div className="flex items-center justify-center gap-1.5">
         <span className="text-3xl font-bold tracking-tighter text-foreground font-headline">
-          FIT
+          Pam
         </span>
         <span className="text-3xl font-semibold tracking-tighter text-primary font-headline">
-          By Pam
+          360
         </span>
       </div>
     </div>
