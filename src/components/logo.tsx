9f689f6c0cc891eecbf73d566a2d1003523f4200
Tegon Fluxo <fluxo.tegon@gmail.com>
2025-09-10 19:48:06 +0000

    mantenha o pam de uma cor e 360 de outra

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ export function Logo({ className }: { className?: string }) {
     <div className={cn("inline-block", className)}>
       <div className="flex items-center justify-center">
         <span className="text-4xl font-bold tracking-wider text-foreground font-headline uppercase">
-          Pam360
+          Pam<span className="text-primary">360</span>
         </span>
       </div>
     </div>
