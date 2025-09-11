49a72cad4e0a4e50c171facc6d19e0ba0cc7b4eb
Tegon Fluxo <fluxo.tegon@gmail.com>
2025-09-11 23:04:26 +0000

    TypeError: Cannot read properties of undefined (reading 'INTERNAL')

--- src/services/firebase-admin.ts
+++ src/services/firebase-admin.ts
@@ -1,7 +1,7 @@
 // src/services/firebase-admin.ts
 import * as admin from 'firebase-admin';
-import { getFirestore } from 'firebase-admin/firestore';
-import { getMessaging } from 'firebase-admin/messaging';
+import { getFirestore, Firestore } from 'firebase-admin/firestore';
+import { getMessaging, Messaging } from 'firebase-admin/messaging';
 
 // Garante que a inicialização ocorra apenas uma vez.
 if (!admin.apps.length) {
@@ -16,8 +16,7 @@ if (!admin.apps.length) {
     }
 }
 
-const app = admin.app();
-const db = getFirestore(app);
-const messaging = getMessaging(app);
+const db: Firestore = getFirestore();
+const messaging: Messaging = getMessaging();
 
 export { db, messaging };
