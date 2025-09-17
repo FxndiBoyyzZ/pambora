e84567dcf0dd7eb362ccf1119e81cac215d8e480
Tegon Fluxo <fluxo.tegon@gmail.com>
2025-09-17 20:37:41 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogTrigger is not

--- src/app/(app)/treinos/page.tsx
+++ src/app/(app)/treinos/page.tsx
@@ -18,6 +18,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 
