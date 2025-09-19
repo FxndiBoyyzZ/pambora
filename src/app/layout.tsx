7f6bed215af132c8e58f3423e6f051cbf7972a7a
Tegon Fluxo <fluxo.tegon@gmail.com>
2025-09-19 20:13:56 +0000

    Log severity: WARNING	Sep 19, 2025, 5:12:56 PM 404 GET https://t-399334

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,6 @@ const bebasNeue = Bebas_Neue({
 export const metadata: Metadata = {
   title: 'Pam Fit - Desafio PAMBORA!',
   description: 'Um aplicativo para o desafio fitness de 21 dias #PAMBORA.',
-  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -30,6 +29,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://www.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link rel="manifest" href="/manifest.json" />
       </head>
       <body className={cn('font-body antialiased', inter.variable, bebasNeue.variable)}>
         <InAppBrowserBlocker />
